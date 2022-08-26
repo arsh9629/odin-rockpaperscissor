3dadd95d9e881c38029d1c4975a85008ceb4c75b
Mohammed Arsh <arz@MohammedArsh.local>
2022-08-26 20:41:06 +0530

    added win and lose count with give chances for 5 times

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 // create a function called getComputerChoice
+let wincount=0
+let losecount=0
 function getComputerChoice(){
     let choice=["rock","paper",'scissor']
    return choice[Math.floor(Math.random()*3)]  
@@ -7,34 +9,45 @@ function getComputerChoice(){
 function play(playerChoice,compChoice){
      playerChoice=playerChoice.toLowerCase()
     if(playerChoice==compChoice){
-        return("It's a tie You both choses"+playerChoice)
+        return(" It's a tie You both choses "+playerChoice)
     }
     else if(playerChoice=="rock"){
         if(compChoice=="paper"){
-            return("You lose!"+compChoice+" beats "+playerChoice)
+            losecount++
+            return("You lose! "+compChoice+" beats "+playerChoice)
         }else{
-            return("You won!"+playerChoice+" beats "+compChoice)
+            wincount++
+            return("You won! "+playerChoice+" beats "+compChoice)
         }
     }else if(playerChoice=="paper"){
         if(compChoice=="scissor"){
-            return("You lose!"+compChoice+" beats "+playerChoice)
+            losecount++
+            return("You lose! "+compChoice+" beats "+playerChoice)
         }
         else{
-            return("You won!"+playerChoice+" beats "+compChoice)
+            wincount++
+            return("You won! "+playerChoice+" beats "+compChoice)
         }
     }else if(playerChoice=="scissor"){
         if(compChoice=="rock"){
-            return("You lose!"+compChoice+" beats "+playerChoice)
+            losecount++
+            return("You lose! "+compChoice+" beats "+playerChoice)
         }else{
-            return("You won!"+playerChoice+" beats "+compChoice)
+            wincount++
+            return("You won! "+playerChoice+" beats "+compChoice)
         }
+    }else{
+        console.log("Wrong Entry")
     }
 }
 
-function game(guess){
-    for(let i=0;i<5;i++){
-        console.log(play(guess,getComputerChoice()))
-    }
+function game(){
+   while((wincount<5)&&(losecount<5)){
+    let guess=prompt("Enter your guess")
+    console.log(play(guess,getComputerChoice()))
+    console.log("Your win count "+wincount)
+    console.log("System's win count "+losecount)
+   }
 }
-let guess=prompt("Enter your guess")
-game(guess)
+
+game()
