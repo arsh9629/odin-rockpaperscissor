10c19422af522facd6233a45fc295c1fe29a4587
Mohammed Arsh <arz@MohammedArsh.local>
2022-09-01 22:32:33 +0530

    user interface added

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ function getComputerChoice(){
 }
 // create a function to play rock paper scissor
 function play(playerChoice,compChoice){
-     playerChoice=playerChoice.toLowerCase()
     if(playerChoice==compChoice){
         return(" It's a tie You both choses "+playerChoice)
     }
@@ -41,13 +40,34 @@ function play(playerChoice,compChoice){
     }
 }
 
-function game(){
-   while((wincount<5)&&(losecount<5)){
-    let guess=prompt("Enter your guess")
-    console.log(play(guess,getComputerChoice()))
-    console.log("Your win count "+wincount)
-    console.log("System's win count "+losecount)
+const guess=function (guess,callback){
+   if((wincount<5)&&(losecount<5)){
+    const status=document.querySelector('.status')
+    status.textContent=callback(guess,getComputerChoice())
+    const win=document.querySelector('.win')
+    win.textContent=wincount
+    const lose=document.querySelector('.lose')
+    lose.textContent=losecount
+    
+   }else{
+       alert("Your Limit is over! Reset to play again")
    }
 }
+const imgs=document.querySelectorAll('img')
+imgs.forEach((img)=>{
 
-game()
+    img.addEventListener('click',get)
+
+})
+function get(){
+    if(this.classList.value=='scissor')return guess('scissor',play)
+    if(this.classList.value=='rock')return guess('rock',play)
+    if(this.classList.value=='paper')return guess('paper',play)
+}
+const rest=document.querySelector('.reset')
+rest.addEventListener('click',reset)
+
+function reset(){
+    window.location.reload()
+}
+
